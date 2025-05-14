24f42b135ea15e2c0cd666cf26dbeb10b19053f2
Ricky-Prophet <studya86308@gmail.com>
2025-05-14 13:58:49 +0530

    Vercel.json

--- client/src/Utilities/Auth/LoginForm.jsx
+++ client/src/Utilities/Auth/LoginForm.jsx
@@ -55,28 +55,29 @@ const LoginForm = () => {
 
     const clearError = () => {
         dispatch(resetAuth());
+        setFormData({ email: '', password: '' });
     };
 
     const { email, password } = formData;
 
     return (
-        <div className="loginForm flex items-center justify-center bg-purple-200 rounded-2xl !p-1">
+        <div className="loginForm flex items-center justify-center bg-purple-200 rounded-2xl p-1">
             <form
                 onSubmit={handleSubmit}
-                className="w-full md:w-5/6 lg:w-4/6 h-full lg:h-5/6 flex flex-col items-center justify-center bg-white rounded-2xl"
+                className="w-full md:w-5/6 lg:w-4/6 h-full lg:h-5/6 flex flex-col items-center justify-center gap-5 bg-white rounded-2xl"
             >
                 <h1 className="font-bold text-2xl mb-4">Login/Sign In</h1>
-                <div className="w-full h-8/12 flex flex-col items-center justify-center gap-2 lg:gap-4 !p-2">
-                    <div className="w-full h-3/12 flex flex-col items-center justify-center">
+                <div className="w-full flex flex-col items-center justify-center gap-4 p-2">
+                    <div className="w-full flex flex-col items-center justify-center">
                         <label
                             htmlFor="email"
-                            className="label w-4/5 lg:w-3/5 h-fit validator border rounded-lg !px-3 flex items-center gap-2"
+                            className="label w-4/5 lg:w-3/5 h-fit border rounded-lg !px-2 flex items-center gap-2"
                         >
                             <MdAttachEmail className="text-xl" />
                             <input
                                 type="email"
                                 id="email"
-                                className="input bg-transparent focus:outline-0 outline-0 border-0 grow"
+                                className="input bg-transparent focus:outline-none border-0 grow"
                                 name="email"
                                 value={email}
                                 onChange={handleChange}
@@ -86,16 +87,16 @@ const LoginForm = () => {
                             />
                         </label>
                     </div>
-                    <div className="w-full h-3/12 flex flex-col items-center justify-center gap-1">
+                    <div className="w-full flex flex-col items-center justify-center">
                         <label
                             htmlFor="password"
-                            className="label w-4/5 lg:w-3/5 h-fit validator border rounded-lg !px-3 flex items-center gap-2"
+                            className="label w-4/5 lg:w-3/5 h-fit border rounded-lg !px-2 flex items-center gap-2"
                         >
                             <RiLockPasswordFill className="text-xl" />
                             <input
                                 type="password"
                                 id="password"
-                                className="input bg-transparent focus:outline-0 outline-0 border-0 grow"
+                                className="input bg-transparent focus:outline-none border-0 grow"
                                 name="password"
                                 value={password}
                                 onChange={handleChange}
@@ -105,11 +106,11 @@ const LoginForm = () => {
                             />
                         </label>
                     </div>
-                    <div className="w-full flex items-center justify-evenly text-gray-400 !mb-2">
+                    <div className="w-full flex items-center justify-evenly text-gray-400 mb-2">
                         <p className="cursor-pointer hover:text-black">Remember Me</p>
                         <p className="cursor-pointer hover:text-black">Forgot Password?</p>
                     </div>
-                    <div className="!my-2">
+                    <div className="my-2">
                         <p
                             className="text-gray-400 cursor-pointer hover:text-black"
                             onClick={() => navigate('/register')}
@@ -129,17 +130,21 @@ const LoginForm = () => {
                             </button>
                         </div>
                     )}
-                    <div className='w-full flex items-center justify-center'>
-                        {loading ? (
-                            <button className="btn w-4/6" disabled>
-                                <span className="loading loading-spinner loading-md"></span>
-                                Loading...
-                            </button>
-                        ) : (
-                            <button type="submit" className="btn w-4/6">
-                                Login
-                            </button>
-                        )}
+                    <div className="w-full flex items-center justify-center">
+                        <button
+                            type="submit"
+                            className="btn w-4/6 flex items-center justify-center"
+                            disabled={loading}
+                        >
+                            {loading ? (
+                                <>
+                                    <span className="loading loading-spinner loading-md mr-2"></span>
+                                    Loading...
+                                </>
+                            ) : (
+                                'Login'
+                            )}
+                        </button>
                     </div>
                 </div>
             </form>
